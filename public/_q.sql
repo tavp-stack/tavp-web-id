8edee31f2bdb2ebe1f0f2f3ba518fa027527261c
JT <me@jtdoank.com>
2026-07-12 08:13:49 +0700

    Fix: analytics chart (pass data to view), PHP timezone Asia/Jakarta, auto-fill author on post save, fix ContentController (restore type() method), Mermaid JS + author display in post.volt

--- public/_q.sql
+++
@@ -1,5 +0,0 @@
-SELECT COUNT(*) as cnt FROM analytics_page_visits;
-SELECT DATE(created_at) as day, COUNT(*) as cnt 
-FROM analytics_page_visits 
-WHERE DATE(created_at) >= DATE_SUB(CURDATE(), INTERVAL 7 DAY) 
-GROUP BY day ORDER BY day;
--- public/_read_content.php
+++
@@ -1,7 +0,0 @@
-<?php
-$file = 'C:\Users\JT\Projects\tavp-web-id\vendor\tavp\cms\src\Admin\ContentController.php';
-$lines = file($file);
-echo "Total lines: " . count($lines) . "\n\n";
-for ($i=54; $i<135 && $i<count($lines); $i++) {
-    echo ($i+1) . ': ' . rtrim($lines[$i]) . "\n";
-}
