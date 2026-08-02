200ceb6cc158aac55bdfeb704fd90df7762adc91
JT <me@jtdoank.com>
2026-08-02 11:59:19 +0700

    fix: session via redis (php-redis + session.save_handler) + secure cookie false for HTTP

--- config/session.php
+++ config/session.php
@@ -33,7 +33,7 @@ return [
 
     'domain' => env('SESSION_DOMAIN'),
 
-    'secure' => env('SESSION_SECURE_COOKIE', true),
+    'secure' => env('SESSION_SECURE_COOKIE', false),
 
     'http_only' => true,
 
